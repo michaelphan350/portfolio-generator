8f2e799ebae7c88d0ed07f6e76965afc64af9e60
Michael <michaelphan350@gmail.com>
2022-06-13 19:21:15 -0700

    added first question

--- app.js
+++ app.js
@@ -10,15 +10,23 @@ const printProfileData = profileDataArr => {
 
 printProfileData(profileDataArgs) */
 
-const fs = require('fs');
-const generatePage = require('./src/page-template.js')
+const inquirer = require('inquirer');
 
-const profileDataArgs =  process.argv.slice(2);
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
-fs.writeFile('./index.html', generatePage(name,github), err => {
-    if(err) throw Error(err);
+// fs.writeFile('./index.html', generatePage(name,github), err => {
+    // if(err) throw Error(err);
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+    // console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+inquirer
+    .prompt ([ {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }
+])
+    .then(answers => console.log(answers));
