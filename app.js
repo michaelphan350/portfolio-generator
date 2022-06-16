1b19906f7299a422a82a3387f59883b80e8216e3
Michael <michaelphan350@gmail.com>
2022-06-15 19:31:19 -0700

    projects section

--- app.js
+++ app.js
@@ -12,16 +12,9 @@ printProfileData(profileDataArgs) */
 
 const inquirer = require('inquirer');
 
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', generatePage(name,github), err => {
-    // if(err) throw Error(err);
-
-    // console.log('Portfolio complete! Check out index.html to see the output!');
-// });
 const promptUser = () => {
     return inquirer.prompt ([ {
         type: 'input',
@@ -150,5 +143,11 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', generatePage(portfolioData), err => {
+            if(err) throw Error(err);
+
+        console.log('Portfolio complete! Check out index.html to see the output!');
+             });
     });
